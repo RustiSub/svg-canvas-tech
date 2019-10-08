04d190ea711e0a4a8d3999570a81e5cbbcd2da1d
wouter.pirotte <wouter.pirotte@tobania.be>
2019-10-08 13:25:57 +0200

    Implemented movement with WAAPI and adjusted with CSS styles.

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function () {
   var width = parent.getBBox().width;
   var height = parent.getBBox().height;
 
-  parent.attr({width: 1400});
+  parent.attr({width: 1900});
   parent.attr({height: height});
 
   var absoluteOrigin = new Vector(parent.getBBox().width / 2, parent.getBBox().height / 2);
@@ -51,88 +51,64 @@ window.addEventListener("load", function () {
   var mouseVector = new Vector(mouse.getBBox().cx, mouse.getBBox().cy);
 
   var pathStartVector = new Vector(pathOffsetTest.getPointAtLength(0).x, pathOffsetTest.getPointAtLength(0).y);
-  var moveMouseVector = pathStartVector.subtract(mouseVector);
+  var animationOriginVector = new Vector(0, 0);
+  var moveMouseVector = animationOriginVector.subtract(mouseVector);
+  mouse.transform('translate(' + moveMouseVector.x + ',' + moveMouseVector.y + ')');
 
-  //mouse.transform('translate(' + moveMouseVector.x + ',' + moveMouseVector.y + ')');
-
-  //mouse.node.style.transform = 'translateX(' + moveMouseVector.x + 'px) translateY(' + moveMouseVector.y + 'px)';
-
-  cameraZoom(10, new Vector(mouse.getBBox().cx, mouse.getBBox().cy));
-  //cameraZoom(10, new Vector(0, height));
-
-  //offset-path: path("M 73.1972 533.559 L 104.153 560.811 L 86.4764 595.327 L 52.1107 558.165 Z");
-
-  //mouse.attr({cx: 73.1972});
-  //mouse.attr({cy: 533.559});
 
   var animation;
-  console.log(parent);
-  console.log(parent.transform());
-
-  console.log(pathOffsetTest);
 
   var aStart = new Vector(0 ,0);
 
-  console.log(aStart);
-  console.log(applyTransform(aStart, parent.transform().globalMatrix));
+
+  console.log(pathOffsetTest.attr('d'));
+
+  // pathOffsetTest.transform('translateX(100px)');
+
+  console.log(pathOffsetTest.attr('d'));
+
+  console.log(pathWalk.attr('d') );
+
+  let duration = 10000;
 
   function mouseWalk() {
-    //mouse.node.style = 'offset-path: path("' + pathOffsetTest.attr('d') + '")';
-    mouse.node.style = 'offset-path: path("m 0,0 100,0")';
+    mouse.node.style = 'offset-path: path("' + pathWalk.attr('d') + '")';
+    //mouse.node.style = 'offset-path: path("m 29.855549,559.21177 0,0")';
+
 
     animation = mouse.node.animate([
       { offsetDistance: 0 },
       { offsetDistance: '100%' }
     ], {
-      duration: 1000,
+      duration: duration,
       easing: 'linear',
-      iterations: 1,
+      iterations: Infinity,
       fill: 'both'
     });
   }
 
-  //mouseWalk();
+  mouseWalk();
 
-  /*  Snap.animate(0, path1Length, function (val) {
-      var pos = pathWalk.getPointAtLength(val);
+  //cameraZoom(10, new Vector(207.2286524689752,123.67886993388154));
+  //cameraZoom(1, new Vector(pathWalk.getPointAtLength(0).x, pathWalk.getPointAtLength(0).y));
 
-      cameraZoom(4, pos);
-    }, 10000);*/
-
-/*  parent.node.animate([
-    {
-      transform: 'scaleY(' + 1 + ') scaleX(' + 1 + ')',
-      transformOrigin: '0px 600px',
-    },
-    {
-      transform: 'scaleY(' + scale + ') scaleX(' + scale + ')',
-      transformOrigin: '0px 600px',
-    },
-    {
-      transform: 'scaleY(' + scale + ') scaleX(' + scale + ')',
-      transformOrigin: '800px 600px',
-    },
-    {
-      transform: 'scaleY(' + scale + ') scaleX(' + scale + ')',
-      transformOrigin: '800px 400px',
-    },
-    {
-      transform: 'scaleY(' + scale + ') scaleX(' + scale + ')',
-      transformOrigin: '0px 400px',
-    },
-    {
-      transform: 'scaleY(' + scale + ') scaleX(' + scale + ')',
-      transformOrigin: '0px 600px',
-    },
-  ], {
-    duration: 10000,
-    iterations: Infinity,
-  });*/
+  console.log(mouse.getBBox());
+  console.log(mouse.transform());
+  console.log(new Vector(mouse.getBBox().cx, mouse.getBBox().cy));
+  console.log(applyTransform(new Vector(mouse.getBBox().cx, mouse.getBBox().cy), mouse.transform().localMatrix));
+  console.log(mouse.transform());
 
   function update(progress) {
     //console.log(animation);
     if (animation && animation.playState === 'running') {
-      //console.log(animation.effect.target.style);
+
+      var iteration = Math.floor(animation.currentTime / duration);
+
+      var pos = ((animation.currentTime / duration) - iteration) * path1Length;
+
+      var cameraVector = new Vector(pathWalk.getPointAtLength(pos).x, pathWalk.getPointAtLength(pos).y);
+
+      cameraZoom(10, cameraVector);
     }
   }
 
